ed754cd1de59d965066e7c4a43d3f62e5703b7f3
asyraffucl <108700037+asyraffucl@users.noreply.github.com>
2022-11-25 06:06:04 +0000

    First commit

---
+++ hardhat.config.ts
@@ -0,0 +1,28 @@
+/* hardhat.config.js */
+require("@nomiclabs/hardhat-waffle");
+
+export{};
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+
+mumbai: {
+    url: "https://rpc-mumbai.maticvigil.com",
+    accounts: process.env.privateKey
+  }
+
+  },
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
